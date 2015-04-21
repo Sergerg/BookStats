88e0baea67331a49d84c498a4a75171a99ee4245
Serger <serger@mifors.com>
2015-04-21 10:13:33 +0400

    Spring security basic

---
+++ src/main/java/org/bookstats/WebSecurityConfig.java
@@ -0,0 +1,48 @@
+package org.bookstats;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Serger
+ * Date: 21.04.2015
+ * Time: 2:44
+ */
+
+@Configuration
+@EnableWebMvcSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated();
+        http
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Configuration
+    protected static class AuthenticationConfiguration extends
+            GlobalAuthenticationConfigurerAdapter {
+
+        @Override
+        public void init(AuthenticationManagerBuilder auth) throws Exception {
+            auth
+                    .inMemoryAuthentication()
+                    .withUser("user").password("password").roles("USER");
+        }
+
+    }
+
+}
---
+++ src/main/java/org/bookstats/web/MvcConfig.java
@@ -0,0 +1,25 @@
+package org.bookstats.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Serger
+ * Date: 21.04.2015
+ * Time: 2:47
+ */
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("/home").setViewName("home");
+//        registry.addViewController("/").setViewName("home");
+//        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
